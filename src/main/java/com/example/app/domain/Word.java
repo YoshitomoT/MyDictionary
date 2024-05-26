fa5d4601a3e9d21ebeebd53be35b3d2bdd119286
YoshitomoT <09c1199@gmail.com>
2024-05-26 15:41:12 +0900

    《生成》Wardクラス（domain）

---
+++ src/main/java/com/example/app/domain/Word.java
@@ -0,0 +1,38 @@
+package com.example.app.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+
+@Data
+public class Word {
+	
+	private Integer id;
+	
+	//登録する辞典名
+	private String dictionaryType;
+	
+	//単語名
+	private String name;
+	
+	//読み方
+	private String pronunciation;
+	
+	//簡単な説明
+	private String simpleDescription;
+	
+	//詳細な説明
+	private String detailDescription;
+	
+	//登録日時
+	private LocalDateTime registeredAt;
+	
+	//更新日
+	private LocalDateTime updatedAt;
+	
+	//閲覧回数
+	private Integer pageViews;
+	
+
+}
