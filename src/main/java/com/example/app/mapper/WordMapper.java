876fb465878cf697a4ab1d307590578e190ae5f3
zd2P07 <zd2P07@example.com>
2024-05-27 15:13:09 +0900

    《修正》Mapperに最終閲覧日を更新するメソッドを追加

--- src/main/java/com/example/app/mapper/WordMapper.java
+++ src/main/java/com/example/app/mapper/WordMapper.java
@@ -11,10 +11,10 @@ public interface WordMapper {
 	
 	List<Word> selectAll();
 
-	Word selectWordById(int wordId);
+	Word selectWordById(Integer wordId);
 
-	Integer incrementPageViews(int wordId);
-	
-	void updaTeWordById(Integer wordId);
+	Integer incrementPageViews(Integer wordId);
+
+	void updateLastViewById(Integer wordId);
 
 }
