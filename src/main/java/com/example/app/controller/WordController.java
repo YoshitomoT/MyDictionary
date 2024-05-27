43da90ab06d29a0bace12186ee2bbc80b6f3e6ad
zd2P07 <zd2P07@example.com>
2024-05-27 11:44:33 +0900

    《修正》リクエストパラメーターの受け取り

--- src/main/java/com/example/app/controller/WordController.java
+++ src/main/java/com/example/app/controller/WordController.java
@@ -3,6 +3,7 @@ package com.example.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.app.service.WordService;
@@ -17,7 +18,7 @@ public class WordController {
 	private final WordService wordService;
 	
 	@GetMapping
-	public String allwords(
+	public String showAlls(
 			Model model
 	) {
 		System.out.println(wordService.getAll());
@@ -25,6 +26,20 @@ public class WordController {
 		//テステス
 		return "all_words";
 	}
+	
+	@GetMapping("/{id}/{name}")
+	public String showDetail(
+			@PathVariable("id") Integer id,
+			@PathVariable("name") String name,
+			Model model
+	) {
+		System.out.println(id);
+		System.out.println(name);
+		// 他の処理やモデルへの追加処理
+		return "word_detail";
+	}
+	
+	
 	
 
 }
