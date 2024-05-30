0cf991142099fffcf8b47649e5780fc4f0505829
zd2P07 <zd2P07@example.com>
2024-05-30 13:43:17 +0900

    《生成》DictMapper：SelectAllメソッド追加

---
+++ src/main/java/com/example/app/mapper/DictMapper.java
@@ -0,0 +1,16 @@
+package com.example.app.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.app.domain.Dictionary;
+
+@Mapper
+public interface DictMapper {
+
+	List<Dictionary> selectAll();
+	
+	
+
+}
