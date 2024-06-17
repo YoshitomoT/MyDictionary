bcea8e145d64294fc8b1f745072a8ef342239ae4
YoshitomoT <09c1199@gmail.com>
2024-06-17 22:15:43 +0900

    《修正》

--- src/main/resources/static/js/tinymce/init-tinymce.js
+++ src/main/resources/static/js/tinymce/init-tinymce.js
@@ -9,11 +9,13 @@
         menubar: false, // メニューバーを表示するかどうか
         plugins: "textcolor", // 文字色、画像ボタン、リンク用のプラグインを適用
         toolbar: [ // ツールバー
-            // 戻る 進む | フォーマット | 太字 斜体 | 左寄せ 中央寄せ 右寄せ 均等割付 | 箇条書き 段落番号 インデントを減らす インデント
-            "undo redo | formatselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",
-            // 文字サイズ 文字色 画像 リンク
-            "fontsizeselect forecolor image link"
-        ],
+			// 戻る 進む | フォーマット | 太字 斜体 | 左寄せ 中央寄せ 右寄せ 均等割付 | 箇条書き 段落番号 インデントを減らす インデント
+			"undo redo | formatselect | fontsize forecolor" 
+			+ "| bold italic" 
+		    + "| alignleft aligncenter alignright alignjustify"
+		    + "| bullist numlist outdent indent"
+		],
         statusbar: false, // ステータスバーを表示するかどうか
+        content_style: "p { margin: 0; }"
     });
 
